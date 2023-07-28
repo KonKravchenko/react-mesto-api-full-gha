14a2bac19e033c0e9c8d3cf31e5ff9b6460616a5
Konstantin Kravchenko <kkrav911@yandex.ru>
2023-07-29 00:01:28 +0300

    Спринт 15

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -98,8 +98,8 @@ class Api {
 // });
 
 export const api = new Api({
-  // url: 'https://api.konkravchenko.nomoreparties.sbs',
-  url: 'http://localhost:3000',
+  url: 'https://api.konkravchenko.nomoreparties.sbs/api',
+  // url: 'http://localhost:3000',
   headers: {
     // 'Authorization': `Bearer ${token}`,
     'Content-Type': 'application/json',
