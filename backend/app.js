830a29115510c21a837e3b5f24e9669c2dbc7009
Konstantin Kravchenko <kkrav911@yandex.ru>
2023-07-30 01:30:03 +0300

    Спринт 15

--- backend/app.js
+++ backend/app.js
@@ -71,11 +71,11 @@ app.use((req, res, next) => {
     return res.end();
   }
 
-  next();
+  return next();
 }
 )
 
-mongoose.connect(DB);
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 // mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
