9f8d4a28f244ded1ca80ebe9d599137e2cf61aca
Konstantin Kravchenko <kkrav911@yandex.ru>
2023-07-30 14:57:50 +0300

    Спринт 15

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -30,13 +30,13 @@ router.post('/signup', celebrate({
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(linkValid),
+    // name: Joi.string().min(2).max(30),
+    // about: Joi.string().min(2).max(30),
+    // avatar: Joi.string().pattern(linkValid),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   })
-    .unknown(true),
+    // .unknown(true),
 }), login);
 
 router.post('/logout', logout);
